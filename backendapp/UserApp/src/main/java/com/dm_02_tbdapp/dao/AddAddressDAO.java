5209b1c69c79c1b25767558af7d847510210ef76
shashi <shashikanthv9@gmail.com>
2018-09-06 11:34:07 +0530

    added a in AddAddresDAO

--- backendapp/UserApp/src/main/java/com/dm_02_tbdapp/dao/AddAddressDAO.java
+++ backendapp/UserApp/src/main/java/com/dm_02_tbdapp/dao/AddAddressDAO.java
@@ -9,7 +9,7 @@ import com.dm_02_tbdapp.bean.User;
 public class AddAddressDAO {
 	Logger logger = LoggerFactory.getLogger(AddAddressDAO.class);
 public void addAddress(User userObj) {
-	// jdbc code to add the address of a requested user with the help of given user object.
+	// jdbc code to add the address of a requested user with the help of given user object a.
 	
 }
 }
